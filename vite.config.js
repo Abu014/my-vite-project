5f57dd2807c509a03c2984f903e30f5264868fd8
timaaaa_ <sadovskiy.timur1301@gmail.com>
2026-05-12 21:53:06 +0700

    Обновление контента

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vite-project/'
+  base: '/my-vite-project/',
 })
