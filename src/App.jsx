e44c341d1ddfb7a42a43fe77a9816d1062aba989
timaaaa_ <sadovskiy.timur1301@gmail.com>
2026-06-14 16:21:09 +0700

    Обновление контента

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
 
   return (
-    <BrowserRouter >
+    <BrowserRouter basename="/my-vite-project">
         <Header />
         <Routes>
             <Route path="/" element={<Screen />} />
